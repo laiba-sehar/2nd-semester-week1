464c132b33f8f445598d36871a1bfe7f4425353b
laiba-sehar <120155099+laiba-sehar@users.noreply.github.com>
2023-04-10 21:04:39 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,74 +5,122 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace fileHandling
+namespace signUp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //task1();
-           // task2();
-            //task3();
-            task5();
+            string path =@"C:\just for practice\file.txt";
+ 
+             string[] name = new string[50];
+            string[] password = new string[50];
+            int option;
+            do
+            {
+                readData(path, name, password);
+                option = MainMenu();
+                if (option == 1)
+                {
 
+                    Console.WriteLine("Enter name");
+                    string n = Console.ReadLine();
+                    Console.WriteLine("Enter password");
+                    string p = Console.ReadLine();
+
+                    signIn(n, p, name, password);
+                }
+                if (option == 2)
+                {
+                    Console.WriteLine("Enter New Name: ");
+                    string n = Console.ReadLine();
+                    Console.WriteLine("Enter New Password: ");
+                    string p = Console.ReadLine();
+                    signUp(path , n , p );
+                }
+
+            }
+            while (option > 3);
+             Console.ReadLine();
         }
-        static void task1()
+        static int MainMenu()
         {
-            string path = "D:\\New folder\\textfile.txt";
-            StreamWriter variable = new StreamWriter(path);
-            variable.WriteLine("hello to the future");
-            variable.WriteLine("My name is laiba");
-            variable.WriteLine("I am the best");
+            int option;
+            Console.WriteLine("1.SignUp");
+            Console.WriteLine("2.SignIn");
+            Console.WriteLine("3.Exit");
+            option = int.Parse(Console.ReadLine());
+            return option;
+        }
+        static void signUp(string path,int n,int p)
+        {
+            StreamWriter variable = new StreamWriter(path,true);
+            variable.WriteLine(n + "," + p);
             variable.Flush();
             variable.Close();
+        }
+        static void signIn(string n,string p,string[] name,string[] password)
+        {
+            bool flag=false;
+            for(int i=0;i<50;i++)
+            {
+                if(n==name[i]&&p==password[i])
+                {
+                    Console.WriteLine("Valid User");
+                    flag = true;
+                }
+                
+            }
+            if(flag==false)
+            
+                {
+                    Console.WriteLine("Invalid User");
+                }
             Console.ReadLine();
 
+
         }
-        static void task2()
+        static string parseData(int field,string record)
         {
-            string path = "D:\\New folder\\newfile.txt";
+            int comma = 1;
+            string item = "";
+            for(int i=0;i<record.Length;i++)
+            {
+                if(record[i]==',')
+                {
+                    comma++;
+                }
+                else if(field==comma)
+                    {
+                    item = item + record[i];
+                }
+            }
+            return item;
+        }
+        static void readData(string path,string[] name,string[] password)
+        {
+            int x = 0;
             if (File.Exists(path))
             {
-                StreamReader variable = new StreamReader(path);
+
+                StreamReader file = new StreamReader(path);
                 string record;
-                while ((record = variable.ReadLine()) != null)
+                while((record = file.ReadLine())!= null)
                 {
-                    Console.WriteLine(record);
+                    name[x] = parseData(record, 1);
+                    password[x] = parseData(record, 2);
+                    x++;
+                    if (x >= 50)
+                    {
+                        break;
+                    }
                 }
-                variable.Close();
+                file.Close();
             }
             else
             {
-                Console.WriteLine("Not exist");
+                Console.WriteLine("Not Exist");
             }
-            Console.ReadLine();
-
         }
-        static void task3()
-        {
-            string path = "D:\\New folder\\newfile.txt";
-                StreamReader variable = new StreamReader(path);
-                string record;
-                while ((record = variable.ReadLine()) != null)
-                {
-                    Console.WriteLine(record);
-                }
-                variable.Close();
-            Console.ReadLine();
-            
-           
-        }
-        static void task5()
-        {
-            string path = "D:\\2\\newfile.txt";
-            StreamWriter variable = new StreamWriter(path);
-            variable.WriteLine("Hello!");
-            variable.Flush();
-            variable.Close();
-            Console.ReadLine();
-
-        }
-
     }
 }
